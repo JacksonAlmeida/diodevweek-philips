9605317b3410fc0f1f8ab970f1bbf50854adc2b2
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-03-09 23:32:01 -0400

    criando os repositorios

---
+++ src/main/java/com/dioclass/devweek/repositories/IncidenciaRepository.java
@@ -0,0 +1,9 @@
+package com.dioclass.devweek.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dioclass.devweek.entities.Incidencia;
+
+public interface IncidenciaRepository extends JpaRepository<Incidencia, Long> {
+
+}
---
+++ src/main/java/com/dioclass/devweek/repositories/RegiaoRepository.java
@@ -0,0 +1,9 @@
+package com.dioclass.devweek.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dioclass.devweek.entities.Regiao;
+
+public interface RegiaoRepository extends JpaRepository<Regiao, Long> {
+
+}
